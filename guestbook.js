ea0968f93a0be17a92896e63b0d10c6f0ec64dfb
walterbaya <56239454+walterbaya@users.noreply.github.com>
2022-06-26 00:57:20 -0300

    Se agrega apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://06b077f3.us-east.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
